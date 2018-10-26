017986ee89b8cdd042fc601d01bd980e5e210546
Garret Blankenship <garret@leafcutter.com.au>
2018-10-26 14:22:33 +1100

    adding in posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_005246) do
+ActiveRecord::Schema.define(version: 2018_10_26_031227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2018_10_26_005246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_avatars_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "image"
+    t.text "caption"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +47,5 @@ ActiveRecord::Schema.define(version: 2018_10_26_005246) do
   end
 
   add_foreign_key "avatars", "users"
+  add_foreign_key "posts", "users"
 end
